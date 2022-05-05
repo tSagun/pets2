fc760f532fad2c38e49907438f7020dce17759a1
tSagun <tobster671@gmail.com>
2022-05-05 13:22:25 -0400

    added vars to sessions array

--- index.php
+++ index.php
@@ -23,14 +23,16 @@ $f3->route('GET /order', function ($f3) {
 
 $f3->route('POST /order2', function ($f3) {
     //echo '<h1>Hello world!</h1>';
-    var_dump($_POST);
+//    var_dump($_POST);
+    $_SESSION['petType'] = $_POST['petType'];
+    $_SESSION['petColor'] = $_POST['petColor'];
     $view = new Template();
     echo $view->render('view/pet-order2.html');
 });
 
 $f3->route('POST /summary', function ($f3) {
     //echo '<h1>Hello world!</h1>';
-
+    $_SESSION['petName'] = $_POST['petName'];
     $view = new Template();
     echo $view->render('view/pet-summary.html');
 });
