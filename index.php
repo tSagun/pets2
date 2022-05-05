6de926504224d3079b7d80dc083ffe0ca997bcde
tSagun <tobster671@gmail.com>
2022-05-05 12:23:59 -0400

    added routes

--- index.php
+++ index.php
@@ -11,7 +11,28 @@ $f3->route('GET /', function ($f3) {
     //echo '<h1>Hello world!</h1>';
 
     $view = new Template();
-    echo "My Pets";
+    echo $view->render('view/pet-home.html');
+});
+
+$f3->route('GET /order', function ($f3) {
+    //echo '<h1>Hello world!</h1>';
+
+    $view = new Template();
+    echo $view->render('view/pet-order1.html');
+});
+
+$f3->route('GET /order2', function ($f3) {
+    //echo '<h1>Hello world!</h1>';
+
+    $view = new Template();
+    echo $view->render('view/pet-order2.html');
+});
+
+$f3->route('GET /summary', function ($f3) {
+    //echo '<h1>Hello world!</h1>';
+
+    $view = new Template();
+    echo $view->render('view/pet-summary.html');
 });
 
 $f3->run();
